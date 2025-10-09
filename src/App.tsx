9325ea453e327d12bb4a5f8d6af34c33caaa462d
Daniel Nguyen <nguyendaniel1312@gmail.com>
2025-10-09 02:56:33 -0700

    add interactions

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,25 @@ const App = () => {
   // const [count, setCount] = useState(0);
   const [selectedAscii, setSelectedAscii] = useState(String);
   const [expandWindow, setExpandWindow] = useState(String);
+  const [selectedWindow, setSelectedWindow] = useState("me");
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const [experienceIndex, setExperienceIndex] = useState(0);
+  const [projectIndex, setProjectIndex] = useState(0);
+
+  const experiences = [
+    "Full-Stack Intern @ TwinMind",
+    "Web Dev Intern @ cooledtured",
+    "SWE Intern @ SCE",
+  ];
+
+  const projects = [
+    "StudyBuddy | 1st @ SCE Hacks 1.0, SJSU",
+    "Chillguy.ai | 2nd @ Hack for Humanity, SCU",
+    "VIVI | HackDavis",
+    "OfficeTracker | CS151 @ SJSU",
+  ];
 
   // pick random ascii art
   useEffect(() => {
@@ -33,6 +52,35 @@ const App = () => {
   //   const id = setInterval(() => setCount((c) => c + 1), 1000);
   //   return () => clearInterval(id);
   // }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (selectedWindow === "experience") {
+        if (e.key === "ArrowUp") {
+          setExperienceIndex((prev) =>
+            prev === 0 ? experiences.length - 1 : prev - 1
+          );
+        } else if (e.key === "ArrowDown") {
+          setExperienceIndex((prev) => (prev + 1) % experiences.length);
+        }
+      } else if (selectedWindow === "projects") {
+        if (e.key === "ArrowUp") {
+          setProjectIndex((prev) =>
+            prev === 0 ? projects.length - 1 : prev - 1
+          );
+        } else if (e.key === "ArrowDown") {
+          setProjectIndex((prev) => (prev + 1) % projects.length);
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [selectedWindow]);
+
+  useEffect(() => {
+    setCurrentIndex(0);
+  }, [selectedWindow]);
 
   const asciiList = [
     `⣶⣶⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠔⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤
@@ -93,18 +141,121 @@ const App = () => {
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-gray-800 text-white">
       {/* Bento box grid */}
-      <div className="grid grid-cols-2 grid-rows-3 w-full mx-1 gap-2 bg-gray-900 rounded-2xl p-1.5 border border-gray-700">
+      <div className="relative grid grid-cols-2 grid-rows-3 w-full mx-1 gap-2 bg-gray-900 rounded-2xl p-1.5 border border-gray-700">
+        {/* Expanded overlay when user clicks */}
+        {expandWindow && (
+          <div className="absolute inset-0 z-20 transition-opacity duration-300">
+            {expandWindow === "me" && (
+              <div className="w-full h-full bg-black border border-gray-700 rounded-xl">
+                <p className="text-black bg-gray-300 rounded-t-xl text-sm text-center relative">
+                  me - zsh
+                  <button className="rounded-full p-1 bg-red-500 absolute right-10 top-1/2 -translate-y-1/2" />
+                  <button
+                    className="rounded-full p-1 bg-yellow-500 absolute right-6 top-1/2 -translate-y-1/2"
+                    onClick={() => setExpandWindow("")}
+                  />
+                  <button
+                    className="rounded-full p-1 bg-green-500 absolute right-2 top-1/2 -translate-y-1/2"
+                    onClick={() => setExpandWindow("me")}
+                  />
+                </p>
+                <div className="my-auto flex">
+                  <p className="text-[4px] text-blue-100 font-mono whitespace-pre min-w-1/2 text-center">
+                    {selectedAscii}
+                  </p>
+                  <div className="mx-auto  min-w-1/2 mt-2">
+                    <p className="text-blue-300 text-sm">daniel@MacbookPro</p>
+                    {/* <p className="ml-4">daniel's website</p> */}
+                    <p className=" ml-4 text-xs">Full Stack</p>
+                    <p className=" ml-4 text-xs">CS @ SJSU</p>
+                    <p className=" ml-4 text-xs">{time.toLocaleTimeString()}</p>
+                  </div>
+                </div>
+              </div>
+            )}
+            {expandWindow === "experience" && (
+              <div className="w-full h-full bg-black border border-gray-700 rounded-xl">
+                <p
+                  className={`text-black rounded-t-xl text-sm text-center relative ${
+                    selectedWindow === "experience" ? "bg-white" : "bg-gray-400"
+                  }`}
+                >
+                  experience - zsh
+                  <p className="rounded-full p-1 bg-red-500 absolute right-10 top-1/2 -translate-y-1/2" />
+                  <p
+                    className="rounded-full p-1 bg-yellow-500 absolute right-6 top-1/2 -translate-y-1/2"
+                    onClick={() => setExpandWindow("")}
+                  />
+                  <p className="rounded-full p-1 bg-green-500 absolute right-2 top-1/2 -translate-y-1/2" />
+                </p>
+                <div className="mt-2 mx-4">
+                  {experiences.map((experience, index) => (
+                    <div
+                      key={index}
+                      className={` rounded-md transition-all duration-150 cursor-pointer ${
+                        index === experienceIndex
+                          ? " text-white"
+                          : "bg-transparent text-blue-300 hover:bg-gray-800"
+                      }`}
+                    >
+                      {experience} {index == experienceIndex ? " <" : ""}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+            {expandWindow === "projects" && (
+              <div className="w-full h-full bg-black border border-gray-700 rounded-xl">
+                <p
+                  className={`text-black rounded-t-xl text-sm text-center relative ${
+                    selectedWindow === "projects" ? "bg-white" : "bg-gray-400"
+                  }`}
+                >
+                  projects - zsh
+                  <p className="rounded-full p-1 bg-red-500 absolute right-10 top-1/2 -translate-y-1/2" />
+                  <p
+                    className="rounded-full p-1 bg-yellow-500 absolute right-6 top-1/2 -translate-y-1/2"
+                    onClick={() => setExpandWindow("")}
+                  />
+                  <p
+                    className="rounded-full p-1 bg-green-500 absolute right-2 top-1/2 -translate-y-1/2"
+                    onClick={() => setExpandWindow("projects")}
+                  />
+                </p>
+                <div className="mt-2 mx-4">
+                  {projects.map((project, index) => (
+                    <div
+                      key={index}
+                      className={` rounded-md transition-all duration-150 cursor-pointer ${
+                        index === projectIndex
+                          ? " text-white"
+                          : "bg-transparent text-blue-300 hover:bg-gray-800"
+                      }`}
+                    >
+                      {project}
+                      {index === projectIndex ? " <" : ""}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        )}
         {/* Main terminal window */}
         <div
           className={` bg-black rounded-xl col-span-2 flex border border-gray-700 flex-col ${
-            expandWindow === "me"
-              ? "row-span-3"
-              : expandWindow && expandWindow !== "me"
-              ? "hidden"
-              : ""
-          }`}
+            expandWindow ? "opacity-0" : ""
+          } transition-opacity duration-500`}
+          onClick={() => {
+            setSelectedWindow("me");
+            console.log(selectedWindow);
+          }}
         >
-          <p className="text-black bg-gray-300 rounded-t-xl text-sm text-center relative">
+          <p
+            className={`text-black  rounded-t-xl text-sm text-center relative ${
+              selectedWindow === "me" ? "bg-white" : "bg-gray-400"
+            }`}
+          >
             me - zsh
             <button className="rounded-full p-1 bg-red-500 absolute right-10 top-1/2 -translate-y-1/2" />
             <button
@@ -133,57 +284,79 @@ const App = () => {
         {/* Experience */}
         <div
           className={` bg-black col-span-2 border border-gray-700 rounded-xl ${
-            expandWindow === "experience"
-              ? "row-span-3"
-              : expandWindow && expandWindow !== "experience"
-              ? "hidden"
-              : ""
-          }`}
+            expandWindow ? "opacity-0" : ""
+          } transition-opacity duration-500`}
+          onClick={() => {
+            setSelectedWindow("experience");
+            console.log(selectedWindow);
+          }}
         >
-          <p className="text-black bg-gray-300 rounded-t-xl text-sm text-center relative">
+          <p
+            className={`text-black rounded-t-xl text-sm text-center relative ${
+              selectedWindow === "experience" ? "bg-white" : "bg-gray-400"
+            }`}
+          >
             experience - zsh
-            <p className="rounded-full p-1 bg-red-500 absolute right-2 top-1/2 -translate-y-1/2" />
-            <p className="rounded-full p-1 bg-green-500 absolute right-10 top-1/2 -translate-y-1/2" />
+            <p className="rounded-full p-1 bg-red-500 absolute right-10 top-1/2 -translate-y-1/2" />
             <p className="rounded-full p-1 bg-yellow-500 absolute right-6 top-1/2 -translate-y-1/2" />
+            <p
+              className="rounded-full p-1 bg-green-500 absolute right-2 top-1/2 -translate-y-1/2"
+              onClick={() => setExpandWindow("experience")}
+            />
           </p>
           <div className="mt-2 mx-4">
-            <p className="text-blue-300 text-sm">
-              Full-Stack Intern @ TwinMind
-            </p>
-            <p className="text-blue-300 text-sm">
-              Web Dev Intern @ cooledtured
-            </p>
-            <p className="text-blue-300 text-sm">SWE Intern @ SCE</p>
+            {experiences.map((experience, index) => (
+              <div
+                key={index}
+                className={` rounded-md transition-all duration-150 cursor-pointer ${
+                  index === experienceIndex
+                    ? " text-white"
+                    : "bg-transparent text-blue-300 hover:bg-gray-800"
+                }`}
+              >
+                {experience} {index == experienceIndex ? " <" : ""}
+              </div>
+            ))}
           </div>
         </div>
 
         {/* Projects */}
         <div
           className={` bg-black col-span-2 border border-gray-700 rounded-xl ${
-            expandWindow === "projects"
-              ? "row-span-3"
-              : expandWindow && expandWindow !== "projects"
-              ? "hidden"
-              : ""
-          }`}
+            expandWindow ? "opacity-0" : ""
+          } transition-opacity duration-500`}
+          onClick={() => {
+            setSelectedWindow("projects");
+            console.log(selectedWindow);
+          }}
         >
-          <p className="text-black bg-gray-300 rounded-t-xl text-sm text-center relative">
+          <p
+            className={`text-black rounded-t-xl text-sm text-center relative ${
+              selectedWindow === "projects" ? "bg-white" : "bg-gray-400"
+            }`}
+          >
             projects - zsh
-            <p className="rounded-full p-1 bg-red-500 absolute right-2 top-1/2 -translate-y-1/2" />
-            <p className="rounded-full p-1 bg-green-500 absolute right-10 top-1/2 -translate-y-1/2" />
+            <p className="rounded-full p-1 bg-red-500 absolute right-10 top-1/2 -translate-y-1/2" />
             <p className="rounded-full p-1 bg-yellow-500 absolute right-6 top-1/2 -translate-y-1/2" />
+            <p
+              className="rounded-full p-1 bg-green-500 absolute right-2 top-1/2 -translate-y-1/2"
+              onClick={() => setExpandWindow("projects")}
+            />
           </p>
           <div className="mt-2 mx-4">
-            <p className="text-blue-300 text-sm">
-              StudyBuddy | 1st @ SCE Hacks 1.0, SJSU
-            </p>
-            <p className="text-blue-300 text-sm">
-              Chillguy.ai | 2nd @ Hack for Humanity, SCU
-            </p>
-            <p className="text-blue-300 text-sm">VIVI | HackDavis</p>
-            <p className="text-blue-300 text-sm">
-              OfficeTracker | CS151 @ SJSU
-            </p>
+            {projects.map((project, index) => (
+              <div
+                key={index}
+                className={` rounded-md transition-all duration-150 cursor-pointer ${
+                  index === projectIndex
+                    ? " text-white"
+                    : "bg-transparent text-blue-300 hover:bg-gray-800"
+                }`}
+              >
+                {project}
+                {index === projectIndex ? " <" : ""}
+              </div>
+            ))}
           </div>
         </div>
         {/* Music */}
